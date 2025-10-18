3954fa701ffaddfaa0642c96686ea41127ff9c1c
onosi <onosi.fumiya@gmail.com>
2025-10-18 15:08:09 +0900

    feat: 最低限の動作確認が完了

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,8 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 /** @type {import("jest").Config} **/
 module.exports = {
   testEnvironment: "node",
+  roots: ['<rootDir>/tests'], // テストディレクトリ指定
+  testMatch: ['**/*.test.ts'], // テストファイルのパターン
   transform: {
     ...tsJestTransformCfg,
   },
